98ff5d69a080833bb616fdf71b1042c9e149b5e8
Yanxi <w1y2f3@126.com>
2022-02-08 08:41:12 -0800

    Initial Commit of HW2

---
+++ HW2-FiniteStateMachine/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager
+{
+   public List<GameObject> Items = new List<GameObject>();
+
+   private void Awake()
+   {
+      Service.ItemManagerInGame = this;
+   }
+
+   public void Creation(GameObject itemObj)
+   {
+      Items.Add(itemObj);
+   }
+
+   public void Destroy()
+   {
+      
+   }
+
+}
